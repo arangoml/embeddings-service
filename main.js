5c905546c370b375c7a5a438addf27dc89bbdaec
Alex Geenen <ageenen@pm.me>
2021-07-07 17:34:04 +0200

    Add List Models Route

--- main.js
+++ main.js
@@ -2,10 +2,13 @@
 
 const {context} = require("@arangodb/locals");
 const createRouter = require("@arangodb/foxx/router");
+const {query} = require("@arangodb");
 const joi = require("joi");
 
 const router = createRouter();
 context.use(router);
+
+const METADATA_DOC_COLLECTION_NAME = "_model_metadata";
 
 router.post("/generate_embeddings", (req, res) => {
     // TODO:
@@ -27,7 +30,24 @@ function generateBatches() {
 
 
 router.get("/models", (_req, res) => {
-    // TODO:
     // Query the model metadata collection and return the results here!
-    throw new Error("Implement me!");
-})
+    const metadata_col = context.collection(METADATA_DOC_COLLECTION_NAME);
+    const model_metadata = query`
+        FOR m in ${metadata_col}
+        RETURN {
+            "name": m.name,
+            "model_type": m.model_type,
+            "emb_dim": m.emb_dim
+        }
+    `.toArray();
+    res.json(model_metadata);
+}).response(
+    200,
+    joi.array().items(
+        joi.object({
+            "name": joi.string(),
+            "model_type": joi.string(),
+            "emb_dim": joi.number()
+        })
+    )
+);
