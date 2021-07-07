92cc6645857594347337e45508495d0afe06f232
Alex Geenen <ageenen@pm.me>
2021-07-07 16:31:44 +0200

    Add Model Metadata Schema Validation

--- scripts/setup.js
+++ scripts/setup.js
@@ -5,13 +5,6 @@ const {context} = require("@arangodb/locals");
 
 const METADATA_DOC_COLLECTION = context.collectionName("_model_metadata");
 
-function createModelMetadataCollection() {
-    if (!db._collection(METADATA_DOC_COLLECTION)) {
-        db._createDocumentCollection(METADATA_DOC_COLLECTION);
-    }
-    return db._collection(METADATA_DOC_COLLECTION);
-}
-
 const modelTypes = {
     WORD_EMBEDDING: "word_embedding_model",
     GRAPH_MODEL: "graph_embedding_model"
@@ -19,26 +12,63 @@ const modelTypes = {
 
 exports.modelTypes = modelTypes;
 
+const modelMetadataSchema = {
+    rule: {
+        "type": "object",
+        "properties": {
+            "model_type": { "enum": [modelTypes.WORD_EMBEDDING, modelTypes.GRAPH_MODEL] },
+            "name": { "type": "string" },
+            "emb_dim": { "type": "number" },
+            "invocation_name": { "type": "string" },
+            "framework": {
+                "type": "object",
+                "properties": {
+                    "name": { "type": "string" }
+                },
+                "required": ["name"]
+            },
+            "website": { "type": "string" }
+        },
+        "required": ["model_type", "name", "emb_dim", "invocation_name", "framework"]
+    },
+    level: "moderate",
+    message: "The model's metadata is invalid"
+};
+
+function createModelMetadataCollection() {
+    if (!db._collection(METADATA_DOC_COLLECTION)) {
+        db._createDocumentCollection(METADATA_DOC_COLLECTION, { "schema": modelMetadataSchema });
+    }
+    return db._collection(METADATA_DOC_COLLECTION);
+}
+
 const seedData = [
     {
-        type: modelTypes.WORD_EMBEDDING,
-        name: "DistilBERT",
-        _key: "DistilBERT",
+        model_type: modelTypes.WORD_EMBEDDING,
+        name: "distilbert-base-uncased",
+        _key: "distilbert-base-uncased",
+        framework: {
+            name: "pytorch"
+        },
         emb_dim: 768,
+        website: "https://huggingface.co/distilbert-base-uncased",
         // This is the name that this model will have on the compute node. May differ from display name
-        invocation_name: "DistilBERT"
+        invocation_name: "distilbert-base-uncased"
     },
     {
-        type: modelTypes.GRAPH_MODEL,
-        name: "GraphSAGE",
-        _key: "GraphSAGE",
+        model_type: modelTypes.GRAPH_MODEL,
+        name: "graph-sage",
+        _key: "graph-sage",
+        framework: {
+            name: "pytorch"
+        },
         emb_dim: 64,
-        invocation_name: "GraphSAGE"
+        invocation_name: "graph-sage"
     }
 ];
 
 function seedMetadataCol(collection) {
-    collection.insert(seedData, { waitForSync: true });
+    collection.insert(seedData);
 }
 
 const modelMetadataCol = createModelMetadataCollection();
