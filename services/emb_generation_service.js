093e43e45d61742443cc46460cd1132776bcc2d2
Alex Geenen <ageenen@pm.me>
2021-07-29 10:51:01 +0200

    Refactor Embeddings Generation

--- services/emb_generation_service.js
+++ services/emb_generation_service.js
@@ -7,16 +7,22 @@ const {query, db} = require("@arangodb");
 
 const embeddingQueueName = "embeddings_generation";
 
-function queueCollectionBatch(i, batchSize, colName, fieldName, modelMetadata, embeddingsQueue, destinationCollection, separateCollection, isLastBatch) {
+const scripts = {
+    NODE: "createNodeEmbeddings",
+    GRAPH: "createGraphEmbeddings"
+};
+
+function queueBatch(scriptName, i, batchSize, graphName, colName, fieldName, modelMetadata, embeddingsQueue, destinationCollection, separateCollection, isLastBatch) {
     embeddingsQueue.push(
         {
             mount: context.mount,
-            name: "createNodeEmbeddings"
+            name: scriptName
         },
         {
             collectionName: colName,
             batchIndex: i,
             modelMetadata,
+            graphName,
             fieldName,
             batchSize,
             destinationCollection,
@@ -26,59 +32,11 @@ function queueCollectionBatch(i, batchSize, colName, fieldName, modelMetadata, e
     );
 }
 
-function queueGraphBatch(i, batchSize, colName, graphName, fieldName, modelMetadata, embeddingsQueue, destinationCollection, separateCollection) {
-    embeddingsQueue.push(
-        {
-            mount: context.mount,
-            name: "createGraphEmbeddings"
-        },
-        {
-            collectionName: colName,
-            batchIndex: i,
-            graphName,
-            modelMetadata,
-            fieldName,
-            batchSize,
-            destinationCollection,
-            separateCollection
-        }
-    );
-}
-
 /**
- * Queue batch jobs to generate embeddings for a specified collection.
+ * Queue batch jobs to generate embeddings for a specified model/scriptType.
  * Returns true if batch jobs have been queued. This does NOT mean that they've succeeded yet.
  */
-function generateBatchesCollection(colName, fieldName, destinationCollection, separateCollection, modelMetadata) {
-    const myCol = db._collection(colName);
-    const numberOfDocuments = query`
-    RETURN COUNT(
-        FOR doc in ${myCol}
-        FILTER doc.${fieldName} != null
-        RETURN 1
-    )
-    `.toArray();
-
-    const batch_size = modelMetadata.metadata.inference_batch_size;
-    const numBatches = Math.ceil(numberOfDocuments / batch_size);
-
-    const embQ = queues.create(embeddingQueueName);
-
-    Array(numBatches)
-        .fill()
-        .map((_, i) => i)
-        .forEach(i => queueCollectionBatch(
-            i, batch_size, colName, fieldName, modelMetadata, embQ, destinationCollection, separateCollection, i == (numBatches - 1)
-        ));
-
-    return true;
-}
-
-/**
- * Queue batch jobs to generate embeddings for a specified graph.
- * Returns true if batch jobs have been queued. This does NOT mean that they've succeeded yet.
- */
-function generateBatchesGraph(graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata) {
+function generateBatches(scriptType, graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata) {
     const myCol = db._collection(collectionName);
     const numberOfDocuments = query`
     RETURN COUNT(
@@ -96,35 +54,38 @@ function generateBatchesGraph(graphName, collectionName, fieldName, destinationC
     Array(numBatches)
         .fill()
         .map((_, i) => i)
-        .forEach(i => queueGraphBatch(
-            i, batch_size, collectionName, graphName, fieldName, modelMetadata, embQ, destinationCollection, separateCollection
+        .forEach(i => queueBatch(
+            scriptType,
+            i,
+            batch_size,
+            collectionName,
+            graphName,
+            fieldName,
+            modelMetadata,
+            embQ,
+            destinationCollection,
+            separateCollection,
+            i === (numBatches - 1)
         ));
-
-    return true;
 }
 
 function generateBatchesForModel(graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata) {
     switch (modelMetadata.model_type) {
         case modelTypes.WORD_EMBEDDING: {
-            const isQueued = generateBatchesCollection(collectionName, fieldName, destinationCollection, separateCollection, modelMetadata);
-            if (isQueued) {
-                return `Queued generation of embeddings for collection ${collectionName} using ${modelMetadata.name} on the ${fieldName} field`;
-            }
-            break;
+            generateBatches(scripts.NODE, graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata);
+            return true;
         }
         case modelTypes.GRAPH_MODEL: {
             if (!graphName) {
                 throw new Error("Requested to generate graph embeddings but no graph is provided");
             }
-            const isQueued = generateBatchesGraph(graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata);
-            if (isQueued) {
-                return `Queued generation of embeddings for collection ${collectionName} traversing ${graphName} using ${modelMetadata.name} on the ${fieldName} field`;
-            }
-            break;
+            generateBatches(scripts.GRAPH, graphName, collectionName, fieldName, destinationCollection, separateCollection, modelMetadata);
+            return true;
         }
         default:
             throw new Error(`Error: unrecognized model type: ${modelMetadata.model_type}`);
     }
+    throw new Error("Unable to queue batches.");
 }
 
 exports.generateBatchesForModel = generateBatchesForModel;
